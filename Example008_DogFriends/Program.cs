256e463b1e491e88c2e40246c1bb0b4827860565
Evgheni Advahov <evghenii.88@gmail.com>
2023-04-06 22:00:55 +0300

    Program dogFriens solved

--- Example008_DogFriends/Program.cs
+++ Example008_DogFriends/Program.cs
@@ -1,12 +1,28 @@
 ﻿
-int count = 0;
-int distance = 10000;
-int firstFriendSpeed = 1;
-int secondFriendSpeed = 2;
-int dogSpeed = 5;
-int friend = 2;
+double count = 0;
+double distance = 10000;
+double firstFriendSpeed = 1;
+double secondFriendSpeed = 2;
+double dogSpeed = 5;
+double friend = 2;
+double time;
 
 while(distance > 10)
 {
-    
+    if(friend == 1)
+    {
+        time = distance/(firstFriendSpeed * dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(secondFriendSpeed * dogSpeed);
+        friend = 1;
+    }
+    distance = distance - ((firstFriendSpeed + secondFriendSpeed) * time);
+    count +=1;
+
 } 
+Console.Write("Собака пробежит: ");
+Console.WriteLine(count);
+ 
