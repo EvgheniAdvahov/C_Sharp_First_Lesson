dbc177c95cb3fa659c744aa08f553650c141430f
Evgheni Advahov <evghenii.88@gmail.com>
2023-05-21 18:58:01 +0300

    Added Last Example_015 from our course C#

---
+++ Example_015_HelloConsole/Program.cs
@@ -0,0 +1,118 @@
+﻿// string path = @"D:\PyCharmProjects\HelloWorld";
+// DirectoryInfo di = new DirectoryInfo(path);
+// System.Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     System.Console.WriteLine(fi[i].Name);
+// }
+
+// void CatalogInfo(string path, string indent ="")
+// {
+//     DirectoryInfo catalog = new DirectoryInfo(path);
+
+//     DirectoryInfo[] catalogs = catalog.GetDirectories();
+//     for (int i = 0; i < catalogs.Length; i++)
+//     {
+//         System.Console.WriteLine($"{indent}{catalogs[i].Name}");
+//         CatalogInfo(catalogs[i].FullName, indent + "  ");
+//     }
+
+//     FileInfo[] files = catalog.GetFiles();
+
+//     for (int i = 0; i < files.Length; i++)
+//     {
+//         System.Console.WriteLine($"{indent}{files[i].Name}");
+//     }
+// }
+
+// string path = @"D:\PyCharmProjects\HelloWorld";
+// CatalogInfo(path);
+
+
+// Ханойские блинчики
+// void Towers(string with = "1", string on = "3", string some = "2", int count =4)
+// {
+//     if (count > 1) Towers(with, some, on, count - 1);
+//     System.Console.WriteLine($"{with} >> {on}");
+//     if (count > 1) Towers(some, on, with, count - 1);
+// }
+// Towers();
+
+// Задача матем
+// string emp = String.Empty;
+// string[] tree = {emp, "/", "*",  "10", "-", "+", emp, emp, "4", "2", "1", "3"};
+// //                0    1    2      3    4    5    6    7    8    9    10   11
+// void InOrderTraversal(int pos = 1)
+// {
+//     if (pos < tree.Length)
+//     {
+//         int left = 2 * pos;
+//         int right = 2 * pos + 1;
+//         if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+//         System.Console.WriteLine(tree[pos]);
+//         if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+//     }
+// }
+// InOrderTraversal();
+
+
+// Сравнение рекурсии и итерации
+// decimal fRec = 0;
+// decimal fIte = 0;
+
+// decimal FibonacciRecursion(int n)
+// {
+//     fRec++;
+//     return n == 0 || n == 1 ? 1 : FibonacciRecursion(n-1) +FibonacciRecursion(n-2);
+// }
+
+// decimal FibonacciIteration(int n)
+// {
+//     fIte++;
+//     decimal result = 1;
+//     decimal f0 = 1;
+//     decimal f1 = 1;
+//     for (int i = 0; i <= n; i++)
+//     {
+//         result = f0 +f1;
+
+//         f0 = f1;
+//         f1 = result;
+//         fIte++;
+//     }
+//     return result;
+// }
+
+// //# 1 2 3 4 5 6
+// //V 1 1 2 3 5 8
+
+// Console.ReadLine();
+// DateTime dt = DateTime.Now; 
+
+// for(int n =10; n < 40; n++)
+// {
+//     System.Console.WriteLine($"FibonacciIteration{n} = {FibonacciIteration(n)} fIte = {fIte.ToString("### ### ###"), -15}");
+//     fIte = 0;
+// }
+// System.Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+// System.Console.WriteLine();
+// Console.ReadLine();
+// dt = DateTime.Now;
+// for (int n = 10; n < 40; n++)
+// {
+//     System.Console.WriteLine($"FibonacciIteration{n} = {FibonacciRecursion(n)} fRec = {fRec.ToString("### ### ###"), -15}");
+//     fIte = 0;
+// }
+// System.Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+
+int i = 0;
+void Rec()
+{
+    System.Console.WriteLine(i++);
+    Rec();
+}
+
+Rec();
---
+++ Example_015_Recur/Program.cs
@@ -0,0 +1,112 @@
+﻿// string NumbersFor(int a, int b)
+// {
+//     string result = String.Empty;
+//     for (int i = a; i <=b; i++)
+//     {
+//         result += $"{i} ";
+//     }
+//     return result;
+// }
+
+
+// // При Рекурсии обязательно надо продумать условие выхода
+// System.Console.WriteLine(NumbersFor(1, 10));
+
+// string NumbersRec(int a, int b)
+// {
+//     if (a < b) return $"{a} " + NumbersRec(a+1, b);
+//     esle:
+//         return String.Empty;
+// }
+// System.Console.WriteLine(NumbersRec(1, 10));
+
+// // Сумма чисел от 1 до N
+
+// int SumFor(int n)
+// {
+//     int result = 0;
+//     for (int i = 1; i <=n; i++) result +=i;
+//     return result;
+// }
+// System.Console.WriteLine(SumFor(4));
+
+// int SumRec(int n)
+// {
+//     if(n == 0) return 0;
+//     else return n + SumRec(n -1);
+// }
+// System.Console.WriteLine(SumRec(5));
+
+// Факториал числа
+
+// int FactorialFor(int n)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= n; i++) result *=i;
+//     return result;
+// }
+// System.Console.WriteLine(FactorialFor(10));
+
+// int FactorialRec(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * FactorialRec(n - 1);
+// }
+// System.Console.WriteLine(FactorialRec(10));
+
+// Вычислить A в степени n
+// int PowerFor(int a, int n)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= n; i++) result *=a;
+//     return result;
+// }
+// System.Console.WriteLine(PowerFor(2, 10));
+
+// int PowerRec(int a, int n)
+// {
+//     // тернарный оператор
+//     return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+//     // if (n == 0 ) return 1;
+//     // else return PowerRec(a, n - 1) * a;
+// }
+// System.Console.WriteLine(PowerRec(2, 10));
+
+// Перебор слов
+
+// char [] s = { 'а', 'и', 'с', 'в'};
+// int count = s.Length;
+// int n = 1;
+// for (int i = 0; i < count; i++)
+// {
+//     for (int j = 0; j < count; j++)
+//     {
+//         for (int k = 0; k < count; k++)
+//         {
+//             System.Console.WriteLine($"{n++, -5}{s[i]}{s[k]}{s[j]}");        
+//         }
+        
+//     }
+    
+// }
+
+// TO DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+// To UNDERSTANDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+// To UNDERSTANDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+// To UNDERSTANDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+// To UNDERSTANDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+// int n = 1;
+// void FindWords(string alphabet, char[] word, int length = 0)
+// {
+//     if (length == word.Length)
+//     {
+//         System.Console.WriteLine($"{n++} {new String(word)}"); return;
+//     }
+//     for (int i = 0; i < alphabet.Length; i++)
+//     {
+//         word[length] = alphabet[i];
+//         FindWords(alphabet, word, length +1);
+//     }
+// }
+
+// FindWords("аисв", new char[2]);
